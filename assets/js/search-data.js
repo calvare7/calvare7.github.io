1de1ecbbbce48ebddbfe5c38e53639050d16c49b
calvare7 <132867610+calvare7@users.noreply.github.com>
2026-03-31 21:47:39 +0000

    Deploying to gh-pages from @ calvare7/calvare7.github.io@7ac11c947178babb1a5460b45fc3d0bdd42c3574 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-new-paper-out-in-nature-communications-aerodynamic-roughness-of-rippled-beds-under-active-saltation-at-earth-to-mars-atmospheric-pressures",
+          title: 'New paper out in Nature Communications! Aerodynamic roughness of rippled beds under active...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
