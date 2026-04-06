564f5039ff3f33c928d641c4ec20b1b90f501a94
calvare7 <132867610+calvare7@users.noreply.github.com>
2026-04-06 04:14:33 +0000

    Deploying to gh-pages from @ calvare7/calvare7.github.io@27252596a88de19fd91f0abb3ea1366be2cdf1b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -449,7 +449,17 @@ ninja.data = [{
           section: "News",},{id: "news-joined-the-fluid-mechanics-research-lab-at-the-university-of-waterloo-as-a-postdoctoral-scholar",
           title: 'Joined the Fluid Mechanics Research Lab at the University of Waterloo as a...',
           description: "",
-          section: "News",},{id: "projects-project-7",
+          section: "News",},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
