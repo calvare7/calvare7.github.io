20d378cd6b776fadc52f3e51e36bd04a960ebdb4
calvare7 <132867610+calvare7@users.noreply.github.com>
2026-04-06 04:13:20 +0000

    Deploying to gh-pages from @ calvare7/calvare7.github.io@a44515c9ca09e05e196f6faf9faaa9d4d7fd311c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -449,12 +449,7 @@ ninja.data = [{
           section: "News",},{id: "news-joined-the-fluid-mechanics-research-lab-at-the-university-of-waterloo-as-a-postdoctoral-scholar",
           title: 'Joined the Fluid Mechanics Research Lab at the University of Waterloo as a...',
           description: "",
-          section: "News",},{id: "projects-subaqueous-barchan-dunes",
-          title: 'Subaqueous Barchan Dunes',
-          description: "Formation and dynamics of barchan dunes under turbulent water flow — lab experiments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
