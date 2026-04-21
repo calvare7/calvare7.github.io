f292b6f72d3fe3afde8a8562bb5aa71e99196fc9
calvare7 <132867610+calvare7@users.noreply.github.com>
2026-04-21 03:05:30 +0000

    Deploying to gh-pages from @ calvare7/calvare7.github.io@76867bdb01d07f805ee24fde8e5cfa8c6ccd28ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "Datasets and scripts to process data from our lab experiments and numerical simulations. Links redirect to respective repositories.",
